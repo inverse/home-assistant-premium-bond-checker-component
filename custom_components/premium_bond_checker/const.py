ba3221905512d04b74359b41e5956d66ea1008ec
Malachi Soord <inverse.chi@gmail.com>
2026-08-23 21:54:50 +0200

    refactor(const): consolidate bond period constants (#116)

--- custom_components/premium_bond_checker/const.py
+++ custom_components/premium_bond_checker/const.py
@@ -1,5 +1,11 @@
 from premium_bond_checker.client import BondPeriod
 
+BOND_PERIOD_CONFIG = {
+    "this_month": (BondPeriod.THIS_MONTH, "This Month"),
+    "last_six_months": (BondPeriod.LAST_SIX_MONTHS, "Last Six Months"),
+    "unclaimed": (BondPeriod.UNCLAIMED, "Unclaimed"),
+}
+
 DOMAIN = "premium_bond_checker"
 
 DEFAULT_SCAN_INTERVAL_WEEKS = 4
@@ -13,16 +19,3 @@ COORDINATOR_NEXT_DRAW = "next_draw"
 ATTR_HEADER = "header"
 ATTR_TAGLINE = "tagline"
 ATTR_REVEAL_BY = "reveal_by"
-
-BOND_PERIODS = {
-    "this_month": BondPeriod.THIS_MONTH,
-    "last_six_months": BondPeriod.LAST_SIX_MONTHS,
-    "unclaimed": BondPeriod.UNCLAIMED,
-}
-
-
-BOND_PERIODS_TO_NAME = {
-    "this_month": "This Month",
-    "last_six_months": "Last Six Months",
-    "unclaimed": "Unclaimed",
-}
